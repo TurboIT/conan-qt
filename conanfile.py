48b894a69e2022da4fbd15f7a4fbfffdf6d0f438
Conrad Jones <cjones@turboit.co.uk>
2018-06-03 19:21:03 +0100

    fix qt version update

--- conanfile.py
+++ conanfile.py
@@ -31,7 +31,7 @@ class QtConan(ConanFile):
     """ Qt Conan package """
 
     name = "Qt"
-    version = "5.8.0"
+    version = "5.11"
     description = "Conan.io package for Qt library."
     source_dir = "qt5"
     settings = "os", "arch", "compiler", "build_type"
@@ -120,10 +120,10 @@ class QtConan(ConanFile):
         major = ".".join(self.version.split(".")[:2])
         self.run("git clone https://code.qt.io/qt/qt5.git")
         self.run("cd %s && git checkout %s" % (self.source_dir, major))
-        self.run("cd %s && perl init-repository --no-update --module-subset=%s"
+        self.run("cd %s && perl init-repository --module-subset=%s"
                  % (self.source_dir, ",".join(submodules)))
-        self.run("cd %s && git checkout v%s && git submodule update"
-                 % (self.source_dir, self.version))
+#        self.run("cd %s && git checkout %s && git submodule update"
+#                 % (self.source_dir, self.version))
 
         if self.settings.os != "Windows":
             self.run("chmod +x ./%s/configure" % self.source_dir)
